fcef050e2fd7d7756c944b7e746cdb01d237ee3d
matildegiuliani <matilde.giuliani1@edu.unifi.it>
2025-05-06 14:25:18 +0200

    Moved to Spreadsheet class

--- main.cpp
+++ main.cpp
@@ -9,65 +9,68 @@
 #include "FoglioCalcolo.h"
 #include <ncurses.h>
 using namespace std;
-void stampaStato(vector<CellaValore*>& celle, CellaSomma* somma, CellaMedia* media, CellaMassimo* massimo, CellaMinimo* minimo) {
-    clear();
-    int riga = 2;
-    for (auto c: celle) {
-        c->show(riga++);
+void stampaStato( vector<CellaValore*>& celle,CellaSomma* somma, CellaMedia* media, CellaMassimo* massimo,
+                  CellaMinimo* minimo, int & riga) {
+    clear ();
+    riga =0;
+    mvprintw(riga++,0,"Stato inziale:");
+    //mvprintw(riga++,0,"Stato:");
+
+    for (auto c : celle) {
+        c->show(riga);
     }
     riga++;
-    somma->showResult(riga++);
-    media->showResult(riga++);
-    massimo->showResult(riga++);
-    minimo->showResult(riga++);
-    mvprintw(riga + 2, 2, "Premi un tasto per continuare...");
-    refresh();
+    somma->showResult(riga);
+    media->showResult(riga);
+    massimo->showResult(riga);
+    minimo->showResult(riga);
+    //mvprintw(riga ++, 0,"%s","-----------" );
+    mvprintw(riga ++,0, "Premi invio per continuare...");
+    refresh ();
     getch();
 }
-
 int main () {
     initscr();
     cbreak();
     noecho();
     curs_set(FALSE);
 
-    CellaValore *a = new CellaValore("A", 1, 10);
-    CellaValore *b = new CellaValore("B", 1, 20);
-    CellaValore *c = new CellaValore("C", 1, 30);
-    vector<CellaValore *> celle = {a, b, c};
+    int riga =0;
+    CellaValore *a = new CellaValore("A", 10);
+    CellaValore *b = new CellaValore("B",20);
+    CellaValore *c = new CellaValore("C", 30);
+    vector < CellaValore * > celle = {a, b, c};
 
-   CellaSomma * somma;
-   /*
-    CellaMedia * media = new CellaMedia("Media", 1, celle);
-    CellaMassimo * massimo = new CellaMassimo("Massimo", 1, celle);
-    CellaMinimo * minimo = new CellaMinimo("Minimo", 1, celle);
-*/
-    FoglioCalcolo * f;
-    f-> addCelleValore (a);
-    f-> addCelleValore(b);
-    f-> addCelleValore(c);
-    somma = f->getSomma();
+    FoglioCalcolo *f = new FoglioCalcolo("Foglio");
 
-    stampaStato(celle, somma, media, massimo, minimo);
+    f->addCellaValore(a);
+    f->addCellaValore(b);
+    f->addCellaValore(c);
+    f->attach ();
+    //f->update (); //prima versione
+
+    stampaStato(celle, f->getSomma(), f->getMedia(),f->getMassimo (),f->getMinimo(), riga);
+    f->update (); //seconda versione
+    stampaStato(celle, f->getSomma(), f->getMedia(),f->getMassimo (),f->getMinimo(), riga);
+
 
     b->setValore(10);
-    stampaStato(celle, somma, media, massimo, minimo);
+    f->update ();
+    stampaStato(celle, f->getSomma(), f->getMedia(),f->getMassimo (),f->getMinimo(),riga);
 
     a->setValore(5);
-    stampaStato(celle, somma, media, massimo, minimo);
-
+    f->update();
+    stampaStato(celle, f->getSomma(), f->getMedia(),f->getMassimo (),f->getMinimo(),riga);
 
     getch();
     endwin();
 
+    delete f;
     delete a;
     delete b;
     delete c;
-    delete somma;
-    delete media;
-    delete massimo;
-    delete minimo;
+
     return 0;
-}
+};
 
 
